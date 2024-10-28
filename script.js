47a9c5cde14a656402e83c9e2b6a5cf50ec2e9b5
swarajmutalik <swarajmutalik@gmail.com>
2024-10-28 15:41:22 +0530

    Simple Calculator

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ let operator = "";
 
 // This is a flag that is used to prevent multiple operators from being pressed consecutively without entering a second number
 let isOperatorActive = false;
+
+// It is a function that is used to update the calculator's display
+// The value that is passed into this function is set as the value of the dsiplay element.
+function updateDisplay(value) {
+  display.value = value;
+}
+
+// It loops through each of the buttons in the nodelist.
+// When a button is clicked, the callback function is triggered.
+// buttonValue is used to store the text content of the button that was clicked.
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.textContent;
+  });
+});
+
+// This logic is for when the clear button is clicked and the following things happen
+// The display is cleared by calling the function - updateDisplay('')
+if (button.id === "clear") {
+  currentInput = "";
+  previousInput = "";
+  operator = "";
+  isOperatorActive = false;
+  updateDisplay("");
+  return;
+}
